30b923abc9325139fc81b3716a67158564e0137f
Gaeun Lee <62734844+Lee-Ga-eun@users.noreply.github.com>
2023-07-23 03:45:40 +0900

    cubit으로 데이터 받아놓기 (기존 코드 주석처리)

--- yoggo/lib/component/home/view/home_screen.dart
+++ yoggo/lib/component/home/view/home_screen.dart
@@ -5,7 +5,195 @@ import 'package:yoggo/size_config.dart';
 import '../../book_intro.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../viewModel/home_screen_cubit.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+//   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+//   Future<void> _sendAnalyticsEvent() async {
+//     try {
+//       // 이벤트 로깅
+//       await analytics.logEvent(
+//         name: 'home_view',
+//         parameters: <String, dynamic>{
+//           //   'string': 'string',
+//           //   'int': 42,
+//           //   'long': 12345678910,
+//           //   'double': 42.0,
+//           //   'bool': true,
+//         },
+//       );
+//     } catch (e) {
+//       // 이벤트 로깅 실패 시 에러 출력
+//       print('Failed to log event: $e');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     _sendAnalyticsEvent();
+//     SizeConfig().init(context);
+//     return Scaffold(
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage('lib/images/bkground.png'),
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         child: SafeArea(
+//           bottom: false,
+//           top: false,
+//           minimum: EdgeInsets.only(left: 3 * SizeConfig.defaultSize!),
+//           child: Column(
+//             children: [
+//               Expanded(
+//                 flex: SizeConfig.defaultSize!.toInt(),
+//                 child: Stack(
+//                   alignment: Alignment.centerLeft,
+//                   children: [
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         Text(
+//                           'LOVEL',
+//                           style: TextStyle(
+//                             fontFamily: 'Modak',
+//                             fontSize: SizeConfig.defaultSize! * 5,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               Expanded(
+//                 flex: SizeConfig.defaultSize!.toInt() * 4,
+//                 child: SingleChildScrollView(
+//                   child: Column(
+//                     children: [
+//                       SizedBox(
+//                         height: SizeConfig.defaultSize! * 28,
+//                         child: BlocProvider(
+//                             create: (context) => DataCubit(),
+//                             child: const DataList()),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       //   ),
+//     );
+//   }
+// }
+
+// class DataList extends StatelessWidget {
+//   const DataList({super.key});
+//   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+//   static Future<void> _sendBookClickEvent(contentId) async {
+//     try {
+//       // 이벤트 로깅
+//       await analytics.logEvent(
+//         name: 'book_click',
+//         parameters: <String, dynamic>{'contentId': contentId},
+//       );
+//     } catch (e) {
+//       // 이벤트 로깅 실패 시 에러 출력
+//       print('Failed to log event: $e');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // 데이터 큐빗을 가져오기.
+//     final dataCubit = context.watch<DataCubit>();
+
+//     // 데이터 큐빗을 통해 데이터를 로드.
+//     dataCubit.loadData();
+
+//     return BlocBuilder<DataCubit, List<BookModel>>(
+//       builder: (context, state) {
+//         if (state.isEmpty) {
+//           return Center(
+//             child: Center(
+//               child: LoadingAnimationWidget.fourRotatingDots(
+//                 color: Colors.white,
+//                 size: SizeConfig.defaultSize! * 16,
+//               ),
+//             ),
+//           );
+//         } else {
+//           return ListView.separated(
+//             scrollDirection: Axis.horizontal,
+//             itemCount: state.length,
+//             itemBuilder: (context, index) {
+//               final book = state[index];
+//               return InkWell(
+//                 onTap: () {
+//                   _sendBookClickEvent(book.id);
+//                   Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => BookIntro(
+//                           title: book.title,
+//                           thumb: book.thumbUrl,
+//                           id: book.id,
+//                           summary: book.summary,
+//                         ),
+//                       ));
+//                 },
+//                 child: Column(
+//                   children: [
+//                     Hero(
+//                       tag: book.id,
+//                       child: Container(
+//                         clipBehavior: Clip.hardEdge,
+//                         decoration: BoxDecoration(
+//                             borderRadius: BorderRadius.circular(10)),
+//                         height: SizeConfig.defaultSize! * 22,
+//                         child: ClipRRect(
+//                           borderRadius: BorderRadius.circular(8),
+//                           child: CachedNetworkImage(
+//                             imageUrl: book.thumbUrl,
+//                             httpHeaders: const {
+//                               "User-Agent":
+//                                   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+//                             },
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(
+//                       height: SizeConfig.defaultSize! * 1,
+//                     ),
+//                     SizedBox(
+//                       width: SizeConfig.defaultSize! * 20,
+//                       child: Text(
+//                         book.title,
+//                         style: TextStyle(
+//                             fontFamily: 'BreeSerif',
+//                             fontSize: SizeConfig.defaultSize! * 1.6),
+//                         textAlign: TextAlign.center,
+//                         maxLines: 2,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               );
+//             },
+//             separatorBuilder: (context, index) =>
+//                 SizedBox(width: 2 * SizeConfig.defaultSize!),
+//           );
+//         }
+//       },
+//     );
+//   }
+// }
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
@@ -33,6 +221,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     _sendAnalyticsEvent();
     SizeConfig().init(context);
+
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
@@ -75,8 +264,10 @@ class HomeScreen extends StatelessWidget {
                       SizedBox(
                         height: SizeConfig.defaultSize! * 28,
                         child: BlocProvider(
-                            create: (context) => DataCubit(),
-                            child: const DataList()),
+                          create: (context) =>
+                              DataCubit()..loadData(), // DataCubit 생성 및 데이터 로드
+                          child: const DataList(),
+                        ),
                       ),
                     ],
                   ),
@@ -94,6 +285,7 @@ class HomeScreen extends StatelessWidget {
 class DataList extends StatelessWidget {
   const DataList({super.key});
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
   static Future<void> _sendBookClickEvent(contentId) async {
     try {
       // 이벤트 로깅
@@ -110,16 +302,18 @@ class DataList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 데이터 큐빗을 가져오기.
-    final dataCubit = context.watch<DataCubit>();
-
-    // 데이터 큐빗을 통해 데이터를 로드.
-    dataCubit.loadData();
+    final dataCubit = BlocProvider.of<DataCubit>(context);
 
     return BlocBuilder<DataCubit, List<BookModel>>(
       builder: (context, state) {
         if (state.isEmpty) {
-          return const Center(
-            child: CircularProgressIndicator(),
+          return Center(
+            child: Center(
+              child: LoadingAnimationWidget.fourRotatingDots(
+                color: Colors.white,
+                size: SizeConfig.defaultSize! * 16,
+              ),
+            ),
           );
         } else {
           return ListView.separated(
@@ -131,15 +325,16 @@ class DataList extends StatelessWidget {
                 onTap: () {
                   _sendBookClickEvent(book.id);
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BookIntro(
-                          title: book.title,
-                          thumb: book.thumbUrl,
-                          id: book.id,
-                          summary: book.summary,
-                        ),
-                      ));
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => BookIntro(
+                        title: book.title,
+                        thumb: book.thumbUrl,
+                        id: book.id,
+                        summary: book.summary,
+                      ),
+                    ),
+                  );
                 },
                 child: Column(
                   children: [
@@ -148,7 +343,8 @@ class DataList extends StatelessWidget {
                       child: Container(
                         clipBehavior: Clip.hardEdge,
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10)),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                         height: SizeConfig.defaultSize! * 22,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -170,8 +366,9 @@ class DataList extends StatelessWidget {
                       child: Text(
                         book.title,
                         style: TextStyle(
-                            fontFamily: 'BreeSerif',
-                            fontSize: SizeConfig.defaultSize! * 1.6),
+                          fontFamily: 'BreeSerif',
+                          fontSize: SizeConfig.defaultSize! * 1.6,
+                        ),
                         textAlign: TextAlign.center,
                         maxLines: 2,
                       ),
@@ -188,177 +385,3 @@ class DataList extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// //import 'package:yoggo/component/login_screen.dart';
-// //import 'package:yoggo/component/purchase.dart';
-// //import 'package:yoggo/component/record_info.dart';
-// import 'package:yoggo/models/webtoon.dart';
-// import 'package:yoggo/component/book_intro.dart';
-// import 'package:yoggo/services/api_service.dart';
-// //import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:yoggo/size_config.dart';
-// import 'package:cached_network_image/cached_network_image.dart';
-// import 'dart:async';
-
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({Key? key}) : super(key: key);
-
-//   @override
-//   _HomeScreenState createState() => _HomeScreenState();
-// }
-
-// class _HomeScreenState extends State<HomeScreen> {
-//   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-//   late Future<List<bookModel>> webtoons;
-//   double dropdownHeight = 0.0;
-//   bool isDataFetched = false; // 데이터를 받아온 여부를 나타내는 플래그
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     webtoons = ApiService.getTodaysToons();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     SizeConfig().init(context);
-//     return Scaffold(
-//       body: Container(
-//         decoration: const BoxDecoration(
-//           image: DecorationImage(
-//             image: AssetImage('lib/images/bkground.png'),
-//             fit: BoxFit.cover,
-//           ),
-//         ),
-//         child: SafeArea(
-//           bottom: false,
-//           top: false,
-//           minimum: EdgeInsets.only(left: 3 * SizeConfig.defaultSize!),
-//           child: Column(
-//             children: [
-//               Expanded(
-//                 flex: SizeConfig.defaultSize!.toInt(),
-//                 child: Stack(
-//                   alignment: Alignment.centerLeft,
-//                   children: [
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.center,
-//                       children: [
-//                         Text(
-//                           'LOVEL',
-//                           style: TextStyle(
-//                             fontFamily: 'Modak',
-//                             fontSize: SizeConfig.defaultSize! * 5,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//               Expanded(
-//                 flex: SizeConfig.defaultSize!.toInt() * 4,
-//                 child: SingleChildScrollView(
-//                   child: Column(
-//                     children: [
-//                       SizedBox(
-//                         height: SizeConfig.defaultSize! * 36,
-//                         child: bookList(),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//       //   ),
-//     );
-//   }
-
-//   Container bookList() {
-//     return Container(
-//       child: FutureBuilder<List<bookModel>>(
-//           future: webtoons,
-//           builder: (context, snapshot) {
-//             if (snapshot.hasData) {
-//               return Column(
-//                 children: [
-//                   Expanded(
-//                     child: ListView.separated(
-//                       scrollDirection: Axis.horizontal,
-//                       itemCount: snapshot.data!.length,
-//                       itemBuilder: (context, index) {
-//                         var book = snapshot.data![index];
-//                         return GestureDetector(
-//                           onTap: () {
-//                             Navigator.push(
-//                                 context,
-//                                 MaterialPageRoute(
-//                                   builder: (context) => BookIntro(
-//                                     title: book.title,
-//                                     thumb: book.thumb,
-//                                     id: book.id,
-//                                     summary: book.summary,
-//                                   ),
-//                                 ));
-//                           },
-//                           child: Column(
-//                             children: [
-//                               Hero(
-//                                 tag: book.id,
-//                                 child: Container(
-//                                   clipBehavior: Clip.hardEdge,
-//                                   decoration: BoxDecoration(
-//                                       borderRadius: BorderRadius.circular(10)),
-//                                   height: SizeConfig.defaultSize! * 22,
-//                                   child: ClipRRect(
-//                                     borderRadius: BorderRadius.circular(8),
-//                                     child: CachedNetworkImage(
-//                                       imageUrl: book.thumb,
-//                                       httpHeaders: const {
-//                                         "User-Agent":
-//                                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-//                                       },
-//                                     ),
-//                                   ),
-//                                 ),
-//                               ),
-//                               SizedBox(
-//                                 height: SizeConfig.defaultSize! * 1,
-//                               ),
-//                               SizedBox(
-//                                 width: SizeConfig.defaultSize! * 20,
-//                                 child: Text(
-//                                   book.title,
-//                                   style: TextStyle(
-//                                       fontFamily: 'BreeSerif',
-//                                       fontSize: SizeConfig.defaultSize! * 1.6),
-//                                   textAlign: TextAlign.center,
-//                                   maxLines: 2,
-//                                 ),
-//                               ),
-//                             ],
-//                           ),
-//                         );
-//                       },
-//                       separatorBuilder: (context, index) =>
-//                           SizedBox(width: 2 * SizeConfig.defaultSize!),
-//                     ),
-//                   ),
-//                 ],
-//               );
-//             } else {
-//               return const Center(
-//                 child: CircularProgressIndicator(
-//                   color: Colors.orange,
-//                 ),
-//               );
-//             }
-//           }),
-//     );
-//   }
-// }
--- yoggo/lib/component/home/viewModel/home_screen_cubit.dart
+++ yoggo/lib/component/home/viewModel/home_screen_cubit.dart
@@ -57,12 +57,13 @@ class BookModel {
 //   }
 // }
 class DataCubit extends Cubit<List<BookModel>> {
+  static bool _isLoaded = false; // 데이터를 로드했는지 여부를 저장하는 변수
+  static List<BookModel> _loadedData = []; // 이전에 로드한 데이터를 저장하는 변수
+
   DataCubit() : super([]);
 
-  bool _isLoaded = false; // 데이터를 로드했는지 여부를 저장하는 변수
-  List<BookModel> _loadedData = []; // 이전에 로드한 데이터를 저장하는 변수
-
   void loadData() async {
+    print(_loadedData);
     if (!_isLoaded) {
       // 데이터를 로드하지 않았을 경우에만 로드
       final response =
@@ -81,3 +82,30 @@ class DataCubit extends Cubit<List<BookModel>> {
     }
   }
 }
+// class DataCubit extends Cubit<List<BookModel>> {
+//   DataCubit() : super([]) {
+//     loadData(); // 생성자에서 loadData 호출
+//   }
+
+//   bool _isLoaded = false; // 데이터를 로드했는지 여부를 저장하는 변수
+//   List<BookModel> _loadedData = []; // 이전에 로드한 데이터를 저장하는 변수
+
+//   Future<void> loadData() async {
+//     if (!_isLoaded) {
+//       // 데이터를 로드하지 않았을 경우에만 로드
+//       final response =
+//           await http.get(Uri.parse('https://yoggo-server.fly.dev/content/all'));
+//       if (response.statusCode == 200) {
+//         final jsonData = json.decode(response.body) as List<dynamic>;
+//         final data = jsonData.map((item) => BookModel.fromJson(item)).toList();
+//         _loadedData = data; // 로드한 데이터를 저장
+//         emit(data);
+//         _isLoaded = true; // 데이터를 로드했으므로 플래그를 true로 설정
+//       } else {
+//         emit(_loadedData); // 에러 발생 시 이전에 로드한 데이터를 상태로 유지
+//       }
+//     } else {
+//       emit(_loadedData);
+//     }
+//   }
+// }
